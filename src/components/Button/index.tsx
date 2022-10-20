d436814a255ddb4ed697b42f0f3c006a5c6ca8d6
orhanince <ince.orhan@yandex.com>
2022-10-20 14:02:49 +0300

    Add axios

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,34 @@
 import React, { FC , ReactElement } from "react";
 import {ButtonProps} from "../../types/component-types";
+import Posts from "../../api/Posts";
+import {IPOST} from "../../types/IPOST";
+
+
 
 const Button: FC<ButtonProps> = (props: ButtonProps): ReactElement => {
+    const [posts, setPosts] = React.useState<IPOST[]>([]);
     const  { buttonText } = props;
+    React.useEffect( () => {
+        Posts.getPosts().then( data => {
+            setPosts(data);
+        }).catch(err => {
+            console.log(new Error('Error occurred while retrieving data from the url!'));
+        });
+    },[]);
+
+
     return (
         <>
+            {posts.map(item => {
+                return (
+                   <div key={item.id}>
+                       {item.userId}
+                       {item.id}
+                       {item.title}
+                       {item.body}
+                   </div>
+                )
+            })}
             {buttonText}
         </>
     )
